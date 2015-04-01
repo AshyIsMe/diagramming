21035c0ebe70e0620f7c517eb207d0043f904547
Aaron Ash <aaron.ash@gmail.com>
2015-04-01 23:09:13 +0800

    wx hello world

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,15 @@
+import Distribution.MacOSX
 import Distribution.Simple
-main = defaultMain
+
+main = defaultMainWithHooks $ simpleUserHooks {
+         postBuild = appBundleBuildHook guiApps
+       }
+
+guiApps :: [MacApp]
+guiApps = [MacApp "diagramming"
+                  (Just "resources/WxHello.icns")
+                  Nothing   -- Default Info.plist
+                  []        -- No other resources
+                  []        -- No other binaries
+                  DoNotChase
+          ]
