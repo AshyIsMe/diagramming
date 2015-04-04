85fc4ffb314052d65fa279d4c9b173ce3f9fc518
Aaron Ash <aaron.ash@gmail.com>
2015-04-05 01:20:33 +0800

    Rendering diagrams correctly to wx panel

--- Main.hs
+++ Main.hs
@@ -1,12 +1,12 @@
 module Main where
-
 import           Codec.Picture
 import           Data.List.Split (chunksOf)
+import qualified Data.Vector.Storable as V (Vector,toList)
+import           Data.Bits
 import qualified Diagrams.Prelude as D
 import qualified Diagrams.Backend.Rasterific as DR
 import           Graphics.UI.WX
 import           Graphics.UI.WXCore.WxcTypes
-import qualified Data.ByteString.Lazy as B
 
 main :: IO ()
 main = do
@@ -21,7 +21,7 @@ hello = do
   p         <- panel f [on paint := paintBalls]
   runButton <- button f [text := "Run", on command := run editor]
   quit      <- button f [text := "Quit", on command := close f]
-  set f [layout := minsize (sz 400 300) $
+  set f [layout := minsize (sz 640 480) $
                    margin 10 (column 5 [fill $ floatCentre (widget p)
                                       ,fill $ floatCentre (widget editor)
                                       ,(row 2 [floatCentre (widget runButton)
@@ -36,29 +36,22 @@ run e = do
 paintBalls :: DC a -> Rect -> IO ()
 paintBalls  dc viewArea
   = do set dc [brushColor := red, brushKind := BrushSolid]
-       testcircle <- imageCreateFromPixels (Size 100 100) $ renderToWxImage testimage 100
+       testcircle <- imageCreateFromPixels (Size 100 100) testimagecolors
        drawImage dc testcircle (Point 0 0) []
 
-c :: D.Diagram DR.B D.R2
-{-c = (D.square (sqrt 2) # D.fc D.red) `D.atop` D.circle 1 # D.fc D.green-}
-c = (D.text "yo" # D.fc D.blue) `D.atop` (D.square (sqrt 2) # D.fc D.red) `D.atop` D.unitCircle # D.fc D.green
+diagram :: D.Diagram DR.B D.R2
+{-diagram = (D.square (sqrt 2) # D.fc D.red) `D.atop` D.circle 1 # D.fc D.green-}
+diagram = (D.text "yo" # D.fc D.blue) `D.atop` (D.square (sqrt 2) # D.fc D.red) `D.atop` D.unitCircle # D.fc D.green
 
 testimage :: Image PixelRGBA8
-{-testimage = D.renderDia DR.Rasterific (DR.RasterificOptions (D.Width 50)) c-}
-testimage = D.renderDia DR.Rasterific (DR.RasterificOptions (D.Dims 100 100)) c
+testimage = D.renderDia DR.Rasterific (DR.RasterificOptions (D.Dims 100 100)) diagram
 
-renderToWxImage :: Image PixelRGBA8 -> Int -> [Color]
-renderToWxImage i width = do
-  case encodeDynamicBitmap $ ImageRGBA8 i of
-    Right bs -> byteStringToColorList bs width
-    Left e -> error e
-
-byteStringToColorList :: B.ByteString -> Int -> [Color]
-byteStringToColorList bs width = _byteStringToColorList wordlist
-  where wordlist = concat . reverse $ chunksOf width $ B.unpack bs
-
-_byteStringToColorList :: [Word8] -> [Color]
-_byteStringToColorList (b:g:r:a:ws) = [colorRGBA r g b a] ++ _byteStringToColorList ws
-_byteStringToColorList [] = error "_byteStringToColorList []"
-
-
+imaged :: V.Vector Word8
+imaged = imageData testimage
+--Pack 4 Word8s into one Word and then construct a Color
+testimagecolors :: [Color]
+testimagecolors = map (Color . buildWord) $ chunksOf 4 $ V.toList $ imageData testimage
+  where buildWord [a,b,c,d] = (fromIntegral a `shiftL` 24) .|. 
+                              (fromIntegral b `shiftL` 16) .|. 
+                              (fromIntegral c `shiftL` 8) .|. 
+                              fromIntegral d
