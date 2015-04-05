e26e6b6fe728957862b9e3a42c38e2ccc7662799
Aaron Ash <aaron.ash@gmail.com>
2015-04-05 15:57:27 +0800

    Working! Run button runs text from TextCtrl

--- Main.hs
+++ Main.hs
@@ -19,30 +19,29 @@ deriving instance Typeable D.Any
 code :: String
 code = "circle 1 :: Diagram B R2"
 
-diagramInterpreter :: H.Interpreter (D.Diagram DR.B D.R2)
-diagramInterpreter =  do
+diagramInterpreter :: String -> H.Interpreter (D.Diagram DR.B D.R2)
+diagramInterpreter diagramhaskell = do
   H.setImportsQ [("Prelude", Nothing), ("Diagrams.Prelude", Nothing), ("Diagrams.Backend.Rasterific", Nothing)]
-  H.interpret "circle 1 :: Diagram B R2" (H.as :: D.QDiagram DR.B D.R2 D.Any)
+  H.interpret diagramhaskell (H.as :: D.QDiagram DR.B D.R2 D.Any)
 
 main :: IO ()
 main = do
   {-writeBitmap "circle.bmp" testimage-}
   savePngImage "testimage.png" $ ImageRGBA8 testimage
-  {-r <- H.runInterpreter $ H.loadModules ["Prelude", "Diagrams.Prelude", "Diagrams.Backend.Rasterific"]-}
-                            {->> H.setImports ["Prelude", "Diagrams.Prelude", "Diagrams.Backend.Rasterific"]-}
-                            {->> H.interpret "circle 1 :: Diagram B R2" (H.as :: D.QDiagram DR.B D.R2 D.Any)-}
-  r <- H.runInterpreter diagramInterpreter
-  case r of
-    Left e -> do error $ show e
-                 start mainWindow
-    Right is -> do putStrLn "diagram bitches"
-                   start mainWindow
+  start mainWindow
+  {-r <- H.runInterpreter $ diagramInterpreter code-}
+  {-case r of-}
+    {-Left e -> do error $ show e-}
+                 {-start mainWindow-}
+    {-Right is -> do putStrLn "diagram bitches"-}
+                   {-start mainWindow-}
 
 mainWindow :: IO ()
 mainWindow = do 
   f         <- frame [text := "Hello!"]
-  editor    <- textCtrl f [wrap := WrapNone]
+  editor    <- textCtrl f [wrap := WrapNone, text := "circle 1 :: Diagram B R2"]
   p         <- panel f [on paint := paintBalls]
+  {-runButton <- button f [text := "Run", on command := run editor]-}
   runButton <- button f [text := "Run", on command := run editor]
   quit      <- button f [text := "Quit", on command := close f]
   set f [layout := minsize (sz 640 480) $
@@ -52,16 +51,36 @@ mainWindow = do
                                               ,floatCentre (widget quit)])
                                       ])]
 
+testWindow :: (D.Diagram DR.B D.R2) -> IO ()
+testWindow d = do
+  f <- frame [text := "testing window"]
+  p <- panel f [on paint := paintDiagram d]
+  set f [layout := minsize (sz 640 480) $
+                   fill (widget p)]
+
+paintDiagram :: (D.Diagram DR.B D.R2) -> DC a -> Rect -> IO ()
+paintDiagram d dc r = do
+  dia <- imageCreateFromPixels (Size (rectWidth r) (rectHeight r)) $
+            renderDiagram d (rectWidth r) (rectHeight r)
+  drawImage dc dia (Point 0 0) []
+
 run :: Textual w => w -> IO ()
 run e = do
   t <- get e text
   putStrLn t
+  r <- H.runInterpreter $ diagramInterpreter t
+  case r of
+    Left e -> do error $ show e
+                 testWindow $ (D.text "Error Bitches" # D.fc D.red)
+    Right is -> do putStrLn "Running diagram"
+                   testWindow is
+
 
 paintBalls :: DC a -> Rect -> IO ()
-paintBalls  dc viewArea
-  = do set dc [brushColor := red, brushKind := BrushSolid]
-       testcircle <- imageCreateFromPixels (Size 100 100) testimagecolors
-       drawImage dc testcircle (Point 0 0) []
+paintBalls dc viewArea = do 
+  set dc [brushColor := red, brushKind := BrushSolid]
+  testcircle <- imageCreateFromPixels (Size 100 100) testimagecolors
+  drawImage dc testcircle (Point 0 0) []
 
 diagram :: D.Diagram DR.B D.R2
 {-diagram = (D.square (sqrt 2) # D.fc D.red) `D.atop` D.circle 1 # D.fc D.green-}
@@ -70,8 +89,6 @@ diagram = (D.text "yo" # D.fc D.blue) `D.atop` (D.square (sqrt 2) # D.fc D.red) 
 testimage :: Image PixelRGBA8
 testimage = D.renderDia DR.Rasterific (DR.RasterificOptions (D.Dims 100 100)) diagram
 
-imaged :: V.Vector Word8
-imaged = imageData testimage
 --Pack 4 Word8s into one Word and then construct a Color
 testimagecolors :: [Color]
 testimagecolors = map (Color . buildWord) $ chunksOf 4 $ V.toList $ imageData testimage
@@ -79,3 +96,14 @@ testimagecolors = map (Color . buildWord) $ chunksOf 4 $ V.toList $ imageData te
                               (fromIntegral b `shiftL` 16) .|. 
                               (fromIntegral c `shiftL` 8) .|. 
                               fromIntegral d
+
+renderDiagram :: (D.Diagram DR.B D.R2) -> Int -> Int -> [Color]
+renderDiagram d w h = do
+  map (Color . buildWord) $ chunksOf 4 $ V.toList $ imageData image
+    where buildWord [a,b,c,d] = (fromIntegral a `shiftL` 24) .|. 
+                                (fromIntegral b `shiftL` 16) .|. 
+                                (fromIntegral c `shiftL` 8) .|. 
+                                fromIntegral d
+          image = D.renderDia DR.Rasterific (DR.RasterificOptions (D.Dims width height)) d
+          width = fromIntegral w
+          height = fromIntegral h
