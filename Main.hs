65ea474c8001a81e238fd20dd406a5c0eed1dff4
Aaron Ash <aaron.ash@gmail.com>
2015-04-05 13:44:57 +0800

    Evaluating diagrams

--- Main.hs
+++ Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable, StandaloneDeriving #-}
+
 module Main where
 import           Codec.Picture
 import           Data.List.Split (chunksOf)
@@ -7,15 +10,36 @@ import qualified Diagrams.Prelude as D
 import qualified Diagrams.Backend.Rasterific as DR
 import           Graphics.UI.WX
 import           Graphics.UI.WXCore.WxcTypes
+import           Data.Typeable
+import qualified Language.Haskell.Interpreter as H
+
+
+deriving instance Typeable D.Any
+
+code :: String
+code = "circle 1 :: Diagram B R2"
+
+diagramInterpreter :: H.Interpreter (D.Diagram DR.B D.R2)
+diagramInterpreter =  do
+  H.setImportsQ [("Prelude", Nothing), ("Diagrams.Prelude", Nothing), ("Diagrams.Backend.Rasterific", Nothing)]
+  H.interpret "circle 1 :: Diagram B R2" (H.as :: D.QDiagram DR.B D.R2 D.Any)
 
 main :: IO ()
 main = do
   {-writeBitmap "circle.bmp" testimage-}
   savePngImage "testimage.png" $ ImageRGBA8 testimage
-  start hello
+  {-r <- H.runInterpreter $ H.loadModules ["Prelude", "Diagrams.Prelude", "Diagrams.Backend.Rasterific"]-}
+                            {->> H.setImports ["Prelude", "Diagrams.Prelude", "Diagrams.Backend.Rasterific"]-}
+                            {->> H.interpret "circle 1 :: Diagram B R2" (H.as :: D.QDiagram DR.B D.R2 D.Any)-}
+  r <- H.runInterpreter diagramInterpreter
+  case r of
+    Left e -> do error $ show e
+                 start mainWindow
+    Right is -> do putStrLn "diagram bitches"
+                   start mainWindow
 
-hello :: IO ()
-hello = do 
+mainWindow :: IO ()
+mainWindow = do 
   f         <- frame [text := "Hello!"]
   editor    <- textCtrl f [wrap := WrapNone]
   p         <- panel f [on paint := paintBalls]
