7267cdf821b02ecfb5bfc5204a3b6705388f22a3
Aaron Ash <aaron.ash@gmail.com>
2015-04-21 16:40:21 +0800

    Doing some stuff...

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ module Main where
 import           Codec.Picture
 import qualified Control.Monad.State as S
 import           Data.List.Split (chunksOf)
+import qualified Data.Map.Strict as Map
 import qualified Data.Vector.Storable as V (toList)
 import           Data.Bits
 import qualified Diagrams.Prelude as D
@@ -17,6 +18,9 @@ import qualified Language.Haskell.Interpreter as H
 
 deriving instance Typeable D.Any
 
+--AA TODO: Sort out a way to pass state between invocations of the update() function the user defines
+type DiagramState = S.State (Map.Map String Double) (D.Diagram DR.B D.R2)
+
 exampleDiagram :: String
 {-exampleDiagram = "circle 0.5 # fc yellow `atop` circle 1 # fc green `atop` square 2 :: Diagram B R2"-}
 exampleDiagram = "beside (r2 (1,1)) (circle 1 # fc yellow) (square 2 # fc green) :: Diagram B R2"
@@ -27,8 +31,7 @@ animateDiagram :: String
 animateDiagram = "blah :: State (Double, Double) (Diagram B R2) -> State (Double, Double) (Diagram B R2)"
 
 main :: IO ()
-main = do
-  start mainWindow
+main = start mainWindow
 
 mainWindow :: IO ()
 mainWindow = do 
@@ -40,8 +43,7 @@ mainWindow = do
   quit           <- button f [text := "Quit", on command := close f]
   set f [layout := minsize (sz 640 480) $
                    margin 10 (column 5 [fill $ floatCentre (widget editor)
-                                      ,(row 2 [floatCentre (widget runButton)
-                                              ,floatCentre (widget quit)])
+                                      ,row 2 [floatCentre (widget runButton) ,floatCentre (widget quit)]
                                       ,fill $ floatCentre (widget animatedEditor)
                                       ,floatCentre (widget runTestButton)
                                       ])]
@@ -57,7 +59,7 @@ renderAnimation = do
   vNum <- varCreate 0.1
   f <- frame [text := "testing window"]
   p <- panel f [on paint := paintAnimation vNum]
-  t <- timer f [interval := 20, on command := nextNum vNum p]
+  t <- timer f [interval := 60, on command := nextNum vNum p]
   set f [layout := minsize (sz 640 480) $
                    fill (widget p)]
 
@@ -65,7 +67,7 @@ paintAnimation :: Var Double -> DC a -> Rect -> IO ()
 paintAnimation vNum dc r = do
   num <- varGet vNum
   dia <- imageCreateFromPixels (Size (rectWidth r) (rectHeight r)) $
-            let d = D.beside (D.r2 (num,1)) (D.circle 1 D.# D.fc D.yellow) (D.square 2 D.# D.fc D.green) :: D.Diagram DR.B D.R2
+            let d = D.beside (D.r2 (num,1)) (D.circle 1 D.# D.fc D.yellow) (D.circle 1 D.# D.fc D.green) :: D.Diagram DR.B D.R2
             in renderDiagram d (rectWidth r) (rectHeight r)
   drawImage dc dia (Point 0 0) []
 
@@ -74,14 +76,14 @@ nextNum vNum p = do
   varUpdate vNum (+ 0.1)
   repaint p
 
-renderWindow :: (D.Diagram DR.B D.R2) -> IO ()
+renderWindow :: D.Diagram DR.B D.R2 -> IO ()
 renderWindow d = do
   f <- frame [text := "testing window"]
   p <- panel f [on paint := paintDiagram d]
   set f [layout := minsize (sz 640 480) $
                    fill (widget p)]
 
-paintDiagram :: (D.Diagram DR.B D.R2) -> DC a -> Rect -> IO ()
+paintDiagram :: D.Diagram DR.B D.R2 -> DC a -> Rect -> IO ()
 paintDiagram d dc r = do
   dia <- imageCreateFromPixels (Size (rectWidth r) (rectHeight r)) $
             renderDiagram d (rectWidth r) (rectHeight r)
@@ -93,8 +95,8 @@ run e = do
   putStrLn t
   r <- H.runInterpreter $ diagramInterpreter t
   case r of
-    Left e -> do putStrLn $ (show e) 
-                 renderWindow $ (D.text (show e) # D.fc D.red)
+    Left e -> do print e
+                 renderWindow (D.text (show e) # D.fc D.red)
     Right is -> do putStrLn "Running diagram"
                    renderWindow is
 
@@ -104,8 +106,8 @@ diagramInterpreter diagramhaskell = do
   H.interpret diagramhaskell (H.as :: D.QDiagram DR.B D.R2 D.Any)
 
 
-renderDiagram :: (D.Diagram DR.B D.R2) -> Int -> Int -> [Color]
-renderDiagram d w h = do
+renderDiagram :: D.Diagram DR.B D.R2 -> Int -> Int -> [Color]
+renderDiagram d w h = 
   map (Color . buildWord) $ chunksOf 4 $ V.toList $ imageData image
           --Pack 4 Word8s into one Word and then construct a Color
     where buildWord [a,b,c,d] = (fromIntegral a `shiftL` 24) .|. 
